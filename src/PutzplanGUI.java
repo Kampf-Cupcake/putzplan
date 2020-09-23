be92920578c5e942baff4cb95c8420d86e6f1219
Kampf-Cupcake <inalan93@googlemail.com>
2020-09-23 18:32:49 +0200

    icons aufgeräumt und gui

--- src/PutzplanGUI.java
+++ src/PutzplanGUI.java
@@ -15,7 +15,7 @@ public class PutzplanGUI extends JFrame implements ActionListener {
 		JButton kalendarMB = new JButton();
 	
 	JPanel haupt = new JPanel();
-		JLabel willkommen;
+		//JLabel willkommen;
 		
 		JPanel benutzerHin = new JPanel();
 			JLabel benutzer;
@@ -47,66 +47,93 @@ public class PutzplanGUI extends JFrame implements ActionListener {
 		
 		Insets insets = hintergrund.getInsets();
 		
-	//Menüfläche
+	//Menuefaeche
 		menu.setBounds(0 + insets.left, 0 + insets.top, 200, 900);
 			
 		menu.setLayout(new BoxLayout(menu, BoxLayout.Y_AXIS));
-			benutzerMB.setIcon(new ImageIcon("bilder\\icons\\menu_benutzer2.png"));
+			benutzerMB.setIcon(new ImageIcon("bilder\\icons\\menu_benutzer3.png"));
+			benutzerMB.setMargin(new Insets(0, 0, 0, 0));
 			menu.add(benutzerMB);
 			benutzerMB.addActionListener(this);
 			
-			aufgabenMB.setIcon(new ImageIcon("bilder\\icons\\menu_aufgaben2.png"));
+			aufgabenMB.setIcon(new ImageIcon("bilder\\icons\\menu_aufgaben3.png"));
+			aufgabenMB.setMargin(new Insets(0, 0, 0, 0));
 			menu.add(aufgabenMB);
 			aufgabenMB.addActionListener(this);
 			
-			kalendarMB.setIcon(new ImageIcon("bilder\\icons\\menu_kalendar2.png"));
+			kalendarMB.setIcon(new ImageIcon("bilder\\icons\\menu_kalendar3.png"));
+			kalendarMB.setMargin(new Insets(0, 0, 0, 0));
 			menu.add(kalendarMB);
 			kalendarMB.addActionListener(this);
 		
 				
 		haupt.setBounds(200 + insets.left, 0 + insets.top, 1000, 900);
 		//haupt.setLayout(null);
-		haupt.setBackground(Color.white);
+		//haupt.setBackground(Color.white);
 		
-	//Hauptfläche:Willkommen/Start-Teil
+	//Hauptflaeche:Kalendar-Teil
+		ImageIcon kalendarH= new ImageIcon("bilder\\icons\\Hintergruende_kalendar.png");
+		kalendar = new JLabel(kalendarH);
+		haupt.add(kalendar);
+		//kalendar.setBounds(0 + insets.left, 0 + insets.top, 1000, 900);
+		kalendar.setVisible(true);
+		
+	/**Hauptfläche:Willkommen/Start-Teil
 			ImageIcon start= new ImageIcon("bilder\\icons\\Hintergruende_willkommen.png");
 			willkommen = new JLabel(start);
 			haupt.add(willkommen);
-			willkommen.setVisible(true);
+			willkommen.setVisible(true);*/
 			
 			
-	//Hauptfläche:Benutzer-Teil
+	//Hauptflaeche:Benutzer-Teil
 			
 			haupt.add(benutzerHin);
-				ImageIcon benutzerH= new ImageIcon("bilder\\icons\\Hintergruende_benutzer.png");
-				benutzer = new JLabel(benutzerH);
-				benutzerHin.add(benutzer);
+				//benutzerHin.setBounds(0 + insets.left, 0 + insets.top, 1000, 900);
+				benutzerHin.setLayout(new BoxLayout(benutzerHin, BoxLayout.X_AXIS));
+					//ImageIcon benutzerH = new ImageIcon("bilder\\icons\\Hintergruende_benutzer.png");
+					//benutzer = new JLabel(benutzerH);
+					//benutzerHin.add(benutzer);
+					//benutzer.setBounds(0 + insets.left, 0 + insets.top, 1000, 900);
+				
+				
+				person1.setIcon(new ImageIcon("bilder\\icons\\plus.png"));
+				person1.setMargin(new Insets(0, 0, 0, 0));
+				benutzerHin.add(person1);
+				//person1.setBounds(0 + insets.left, 333 + insets.top, 333, 900);
+				person1.addActionListener(this);
+				
+				//person1.setVisible(false);
+				person2.setIcon(new ImageIcon("bilder\\icons\\plus.png"));
+				person2.setMargin(new Insets(0, 0, 0, 0));
+				benutzerHin.add(person2);
+				person2.addActionListener(this);
+				
+				person3.setIcon(new ImageIcon("bilder\\icons\\plus.png"));
+				person3.setMargin(new Insets(0, 0, 0, 0));
+				benutzerHin.add(person3);
+				person3.addActionListener(this);
+				
 				benutzerHin.setVisible(false);
-			haupt.add(benutzer1);
 			
-			//benutzer1.setLayout(new BoxLayout(benutzer1, BoxLayout.X_AXIS));
+			/**haupt.add(benutzer1);
+				benutzer1.setBounds(0 + insets.left, 0 + insets.top, 1000, 900);
 				benutzer1.add(person1);
 				person1.addActionListener(this);
 				benutzer1.add(person2);
 				person2.addActionListener(this);
 				benutzer1.add(person3);
 				person3.addActionListener(this);
-				benutzer1.setVisible(false);
+				
+				benutzer1.setVisible(false);*/
 
 			
-	//Hauptfläche:Aufgaben-Teil
+	//Hauptflaeche:Aufgaben-Teil
 			ImageIcon aufgabenH= new ImageIcon("bilder\\icons\\Hintergruende_aufgaben.png");
 			aufgaben = new JLabel(aufgabenH);
 			haupt.add(aufgaben);
+			//aufgaben.setBounds(0 + insets.left, 0 + insets.top, 1000, 900);
 			aufgaben.setVisible(false);
 
-			
-	//Hauptfläche:Kalendar-Teil
-			ImageIcon kalendarH= new ImageIcon("bilder\\icons\\Hintergruende_kalendar.png");
-			kalendar = new JLabel(kalendarH);
-			haupt.add(kalendar);
-			kalendar.setVisible(false);
-		
 
 	}
 	
@@ -123,28 +150,34 @@ public class PutzplanGUI extends JFrame implements ActionListener {
         // Buttons abgeglichen. Wenn die Quelle des ActionEvents einer
         // der Buttons ist, werden die jeweiligen Sachen ausgefüht
         if(ae.getSource() == this.benutzerMB){
-        	willkommen.setVisible(false);
-        	//benutzerHin.setVisible(true);
-        	benutzer1.setVisible(true);
+        	//willkommen.setVisible(false);
+        	benutzerHin.setVisible(true);
+        	//person1.setVisible(true);
 
         	aufgaben.setVisible(false);
         	kalendar.setVisible(false);
         }
         else if(ae.getSource() == this.aufgabenMB){
-        	willkommen.setVisible(false);
+        	//willkommen.setVisible(false);
         	benutzerHin.setVisible(false);
         	benutzer1.setVisible(false);
         	aufgaben.setVisible(true);
         	kalendar.setVisible(false);
         }
         else if (ae.getSource() == this.kalendarMB){
-        	willkommen.setVisible(false);
+        	//willkommen.setVisible(false);
         	benutzerHin.setVisible(false);
         	benutzer1.setVisible(false);
         	aufgaben.setVisible(false);
         	kalendar.setVisible(true);
         }
         else if(ae.getSource() == this.person1) {
+        	
+        }
+        else if(ae.getSource() == this.person2) {
+        	
+        }
+        else if(ae.getSource() == this.person3) {
         	
         }
     
